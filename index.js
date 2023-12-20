b3c96ffd1ee096c19d8dd3268fb489335872ec0e
Surya-MERNstack <charusurya17@gmail.com>
2023-12-20 07:48:18 +0530

    update

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts');
-const categoryRoute = require('./routes/categories');
-const commentRoute = require('./routes/comments');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const commentRoute = require("./routes/comments");
 
 // Load environment variables
 dotenv.config();
 
 // Database connection
-const Db = "mongodb+srv://charusurya17:CharlieDerex17@surya-ecommerce.fkvygn6.mongodb.net/blog_data?retryWrites=true&w=majority";
+const Db =
+  "mongodb+srv://charusurya17:CharlieDerex17@surya-ecommerce.fkvygn6.mongodb.net/blog_data?retryWrites=true&w=majority";
 
-mongoose.connect(Db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(Db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Mongoose connected!');
+    console.log("Mongoose connected!");
   })
   .catch((err) => {
-    console.error('Mongoose connection error:', err);
+    console.error("Mongoose connection error:", err);
   });
 
 // CORS options
-const corsOptions = { 
-  origin: ['https://master--blogworkspace.netlify.app', "http://localhost:5173", "*", "https://blogworkspace.netlify.app", "http://localhost:3000/", "https://shopping-redlitmus.netlify.app/"],
+const corsOptions = {
+  origin: [
+    "*",
+    'https://master--blogworkspace.netlify.app',
+    'http://localhost:5173',
+    'https://blogworkspace.netlify.app',
+    'http://localhost:3000',
+    'https://shopping-redlitmus.netlify.app'
+  ],
   credentials: true,
 };
+
 
 // Middleware
 app.use("/images", express.static(path.join(__dirname, "images")));
@@ -43,10 +53,18 @@ app.use("/api/posts", postRoute);
 app.use("/api/cats", categoryRoute);
 app.use("/api/comments", commentRoute);
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173', "*","https://master--blogworkspace.netlify.app" , "https://blogworkspace.netlify.app", "http://localhost:3000/", "https://shopping-redlitmus.netlify.app/");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:5173",
+    "*",
+    "https://master--blogworkspace.netlify.app",
+    "https://blogworkspace.netlify.app",
+    "http://localhost:3000/",
+    "https://shopping-redlitmus.netlify.app/"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
@@ -58,19 +76,19 @@ app.get("/", (req, res) => {
 // Image upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
     // cb(null, req.body.img);
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const ext = path.extname(file.originalname);
-    const filename = file.fieldname + '-' + uniqueSuffix + ext;
+    const filename = file.fieldname + "-" + uniqueSuffix + ext;
     cb(null, filename);
   },
 });
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
-   console.log(req.body);
+  console.log(req.body);
   res.status(200).json("Image has been uploaded!");
 });
 
